10fedb464790f827ee3f7c2491edaa8c71e34452
Gaurav Kumar daharia <gauravdaharia80@gmail.com>
2024-02-01 00:01:09 +0530

    new file update with strealite

---
+++ stockmarket.py
@@ -0,0 +1,7 @@
+import streamlit as st 
+import pandas as pd 
+import yfinance as yf
+import numpy as np
+
+st.title('Stock Market App')
+st.write('This is my first stock market application using streamlit and I will conquire the world !!!')
